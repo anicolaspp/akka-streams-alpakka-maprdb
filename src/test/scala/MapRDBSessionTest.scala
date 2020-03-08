487688df0a941d03bb490fc6895aaa68fcbf59ed
Nicolas A Perez <anicolaspp@gmail.com>
2020-03-07 22:22:20 -0500

    Update SourceTest.scala

--- src/test/scala/MapRDBSessionTest.scala
+++ src/test/scala/MapRDBSessionTest.scala
@@ -1,47 +1,47 @@
-import com.github.anicolaspp.alpakka.maprdb.MapRDBSession
-import com.github.anicolaspp.ojai.ScalaOjaiTesting
-import org.scalatest.{FlatSpec, Matchers}
-
-class MapRDBSessionTest extends FlatSpec with Matchers with ScalaOjaiTesting {
-
-  import scala.collection.JavaConverters._
-
-  ignore "MapRDBSession" should "always return the same store" in {
-    val session = MapRDBSession(getConnection())
-
-    val storeT1 = session.getStore("table")
-
-    storeT1.insert(getConnection().newDocument().setId("5").set("name", "Joe"))
-
-    session.getStore("table").find().asScala.size should be(1)
-  }
-
-  ignore it should "be able to keep track of multiple stores" in {
-    val session = MapRDBSession(getConnection())
-
-    (1 to 100)
-      .map(n => session.getStore(n.toString))
-      .foreach(_.insert(getConnection().newDocument().setId("5").set("name", "Joe")))
-
-    (1 to 100)
-      .map(n => session.getStore(n.toString))
-      .flatMap(_.find().asScala)
-      .size should be(100)
-  }
-
-  ignore it should "close all open stores" in {
-    val session = MapRDBSession(getConnection())
-
-    (1 to 100)
-      .map(n => session.getStore(n.toString))
-      .foreach(_.insert(getConnection().newDocument().setId("5").set("name", "Joe")))
-
-    session.close()
-
-    getConnection().getStore("1").find().asScala.size should be(0)
-  }
-
-  ignore it should "be a single instance available all the time" in {
-    MapRDBSession(getConnection()) should be(MapRDBSession(getConnection()))
-  }
-}
+//import com.github.anicolaspp.alpakka.maprdb.MapRDBSession
+//import com.github.anicolaspp.ojai.ScalaOjaiTesting
+//import org.scalatest.{FlatSpec, Matchers}
+//
+//class MapRDBSessionTest extends FlatSpec with Matchers with ScalaOjaiTesting {
+//
+//  import scala.collection.JavaConverters._
+//
+//  ignore "MapRDBSession" should "always return the same store" in {
+//    val session = MapRDBSession(getConnection())
+//
+//    val storeT1 = session.getStore("table")
+//
+//    storeT1.insert(getConnection().newDocument().setId("5").set("name", "Joe"))
+//
+//    session.getStore("table").find().asScala.size should be(1)
+//  }
+//
+//  ignore it should "be able to keep track of multiple stores" in {
+//    val session = MapRDBSession(getConnection())
+//
+//    (1 to 100)
+//      .map(n => session.getStore(n.toString))
+//      .foreach(_.insert(getConnection().newDocument().setId("5").set("name", "Joe")))
+//
+//    (1 to 100)
+//      .map(n => session.getStore(n.toString))
+//      .flatMap(_.find().asScala)
+//      .size should be(100)
+//  }
+//
+//  ignore it should "close all open stores" in {
+//    val session = MapRDBSession(getConnection())
+//
+//    (1 to 100)
+//      .map(n => session.getStore(n.toString))
+//      .foreach(_.insert(getConnection().newDocument().setId("5").set("name", "Joe")))
+//
+//    session.close()
+//
+//    getConnection().getStore("1").find().asScala.size should be(0)
+//  }
+//
+//  ignore it should "be a single instance available all the time" in {
+//    MapRDBSession(getConnection()) should be(MapRDBSession(getConnection()))
+//  }
+//}
--- src/test/scala/SourceTest.scala
+++ src/test/scala/SourceTest.scala
@@ -1,100 +1,100 @@
-import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
-import com.github.anicolaspp.alpakka.maprdb.MapRDBSession
-import com.github.anicolaspp.alpakka.maprdb.scaladsl.{MapRDBFlow, MapRDBSource}
-import com.github.anicolaspp.ojai.ScalaOjaiTesting
-import org.ojai.store.QueryCondition
-import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
-
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-
-
-class SourceTest extends FlatSpec with ScalaOjaiTesting with Matchers with BeforeAndAfterEach {
-  import scala.collection.JavaConverters._
-
-
-  ignore it should "read from table" in {
-
-    implicit val system = ActorSystem("test")
-
-//    val connection = getConnection()
-
-    (1 to 100)
-      .map(n => getConnection().newDocument().setId(n.toString).set("value", n))
-      .foreach(d => getConnection().getStore("s").insert(d))
-
-    getConnection.getStore("s").find().asScala.size should be (100)
-
-    println("here...")
-
-    val source = MapRDBSource
-      .fromTable("s", MapRDBSession(getConnection))
-      .map(_.getInt("value"))
-      .runFold(0)((acc, _) => {
-        println(acc)
-
-        acc + 1
-      })
-
-    Await.result(source, Duration.Inf) should be(100)
-  }
-
-  ignore it should "read from table by query" in {
-    implicit val system = ActorSystem("test")
-
-    val ss = Source.fromIterator(() =>
-      (1 to 100)
-        .map(n => getConnection().newDocument().setId(n.toString).set("value", n)).iterator)
-
-    ss.via(MapRDBFlow.upsert(MapRDBSession(getConnection()), "table", 1))
-      .runWith(Sink.ignore)
-
-    val query = getConnection()
-      .newQuery()
-      .where(getConnection()
-        .newCondition()
-        .is("value", QueryCondition.Op.EQUAL, 50)
-        .build()
-      ).build()
-
-    val source = MapRDBSource
-      .fromQuery(query, "table", MapRDBSession(getConnection()))
-      .map(_.getInt("value"))
-      .runFold(0)((acc, _) => acc + 1)
-
-    Await.result(source, Duration.Inf) should be(1)
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//import akka.actor.ActorSystem
+//import akka.stream.scaladsl.{Sink, Source}
+//import com.github.anicolaspp.alpakka.maprdb.MapRDBSession
+//import com.github.anicolaspp.alpakka.maprdb.scaladsl.{MapRDBFlow, MapRDBSource}
+//import com.github.anicolaspp.ojai.ScalaOjaiTesting
+//import org.ojai.store.QueryCondition
+//import org.scalatest.{BeforeAndAfterEach, FlatSpec, Matchers}
+//
+//import scala.concurrent.Await
+//import scala.concurrent.duration.Duration
+//
+//
+//class SourceTest extends FlatSpec with ScalaOjaiTesting with Matchers with BeforeAndAfterEach {
+//  import scala.collection.JavaConverters._
+//
+//
+//  ignore "Source" should "read from table" in {
+//
+//    implicit val system = ActorSystem("test")
+//
+////    val connection = getConnection()
+//
+//    (1 to 100)
+//      .map(n => getConnection().newDocument().setId(n.toString).set("value", n))
+//      .foreach(d => getConnection().getStore("s").insert(d))
+//
+//    getConnection.getStore("s").find().asScala.size should be (100)
+//
+//    println("here...")
+//
+//    val source = MapRDBSource
+//      .fromTable("s", MapRDBSession(getConnection))
+//      .map(_.getInt("value"))
+//      .runFold(0)((acc, _) => {
+//        println(acc)
+//
+//        acc + 1
+//      })
+//
+//    Await.result(source, Duration.Inf) should be(100)
+//  }
+//
+//  ignore it should "read from table by query" in {
+//    implicit val system = ActorSystem("test")
+//
+//    val ss = Source.fromIterator(() =>
+//      (1 to 100)
+//        .map(n => getConnection().newDocument().setId(n.toString).set("value", n)).iterator)
+//
+//    ss.via(MapRDBFlow.upsert(MapRDBSession(getConnection()), "table", 1))
+//      .runWith(Sink.ignore)
+//
+//    val query = getConnection()
+//      .newQuery()
+//      .where(getConnection()
+//        .newCondition()
+//        .is("value", QueryCondition.Op.EQUAL, 50)
+//        .build()
+//      ).build()
+//
+//    val source = MapRDBSource
+//      .fromQuery(query, "table", MapRDBSession(getConnection()))
+//      .map(_.getInt("value"))
+//      .runFold(0)((acc, _) => acc + 1)
+//
+//    Await.result(source, Duration.Inf) should be(1)
+//  }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
